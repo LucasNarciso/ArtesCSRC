4e202545691944b345fd25b347a41ea0374dc628
Lucas Narciso <lucasnarcisoo@gmail.com>
2024-03-05 18:33:46 -0300

    Primeira versão do site

---
+++ js.js
@@ -0,0 +1,10 @@
+function abreEdicaoModelo(idModelo){
+    window.location.href = '/EditarModelo.html'
+    console.log('asdasd')
+}
+
+function addCamposEdit(){
+    document.querySelector('.card-edit').insertAdjacentHTML('afterBegin',`
+        <input class="input-edit-text" type="Text" value="SANTA MISSA" style="position: absolute; top: 119px; left: 26px;"></input>
+    `)
+}
